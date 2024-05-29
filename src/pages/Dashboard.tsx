63a007f254fa71b464af5d385574ec08030b123a
Chayapol Wongpuwarak <kongkcypw@gmail.com>
2024-05-30 01:10:55 +0700

    fix: build-error, never read value

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,32 +1,32 @@
-import React, { useEffect, useState } from 'react'
-import { Movie } from './ManagerMovieGUI'
-import { axiosInstance } from '../api/axios';
+import React, { useEffect } from 'react'
+// import { Movie } from './ManagerMovieGUI'
+// import { axiosInstance } from '../api/axios';
 
 interface DashboardProps { }
 
 const Dashboard: React.FC<DashboardProps> = ({ }) => {
 
-    const [isLoading, setIsLoading] = useState<boolean | null>(null);
-    const [movieList, setMovieList] = useState<Movie[]>([]);
+    // const [isLoading, setIsLoading] = useState<boolean | null>(null);
+    // const [movieList, setMovieList] = useState<Movie[]>([]);
 
     useEffect(() => {
-        fetchAllMovie();
+        // fetchAllMovie();
     }, [])
 
-    const fetchAllMovie = async () => {
-        try {
-          setIsLoading(true);
-          const response = await axiosInstance.get("/api/movie/get-all");
-          if (response.status === 200) {
-            console.log(response.data.movie);
-            setMovieList(response.data.movie);
-            setIsLoading(false);
-          }
-        } catch (error) {
-          console.log(error)
-          setIsLoading(false);
-        }
-      }
+    // const fetchAllMovie = async () => {
+    //     try {
+    //       setIsLoading(true);
+    //       const response = await axiosInstance.get("/api/movie/get-all");
+    //       if (response.status === 200) {
+    //         console.log(response.data.movie);
+    //         setMovieList(response.data.movie);
+    //         setIsLoading(false);
+    //       }
+    //     } catch (error) {
+    //       console.log(error)
+    //       setIsLoading(false);
+    //     }
+    //   }
 
     return (
         <div>Dashboard</div>
