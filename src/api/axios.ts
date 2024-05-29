a6c6ba8c8b5e220bf754334e70b4093f19b53bc4
kongkcypw <90099430+kongkcypw@users.noreply.github.com>
2024-05-29 13:24:54 +0700

    test: endpoint url

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_PRODUCTION_MODE === true
-    ? import.meta.env.VITT_SERVER_HOSTING_URL
-    : import.meta.env.VITE_SERVER_LOCALHOST;
+// const BASE_URL = import.meta.env.VITE_PRODUCTION_MODE === true
+//     ? import.meta.env.VITT_SERVER_HOSTING_URL
+//     : import.meta.env.VITE_SERVER_LOCALHOST;
 
 const axiosInstance = axios.create({
     withCredentials: true,
